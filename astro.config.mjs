ed60c1a9d065a0b3b5476bd4c2673304a5ff1e44
Jide Muritala <115728688+Jide-Muritala@users.noreply.github.com>
2024-03-08 17:38:33 +0000

    Add preact to change greeting on homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import vercel from "@astrojs/vercel/serverless";
+
+import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  site: "https://better-belt.vercel.app/"
+  site: "https://better-belt.vercel.app/",
+  integrations: [preact()]
 });
